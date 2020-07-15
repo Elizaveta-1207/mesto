75f3c90027a4fb3043d4e2ed78469e5f914cad80
Elizaveta Timonina <elizaveta1207@gmail.com>
2020-07-15 16:17:28 +0300

    feat: add open and close functions for popup-add

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,58 @@
-let main = document.querySelector(".main");
-let editButton = main.querySelector(".profile__edit-button");
-let popup = document.querySelector(".popup");
-let closeButton = popup.querySelector(".popup__close-button");
-let formElement = popup.querySelector(".popup__form");
+const main = document.querySelector(".main");
+const editButton = main.querySelector(".profile__edit-button");
+const addButton = main.querySelector(".profile__add-button");
+
+// let popup = document.querySelector(".popup");
+const popup = Array.from(document.querySelectorAll(".popup"));
+const editPopup = document.querySelector(".popup-edit");
+const addPopup = document.querySelector(".popup-add");
+
+// let closeButton = popup.querySelector(".popup__close-button");
+const closeButton = popup.map(item => item.querySelector(".popup__close-button"));
+
+
+// let formElement = document.querySelector(".popup-edit__form");
+const formElement = popup.find(item => item.querySelector(".popup-edit__form"));
+// let formElement = popup.forEach(item => item.querySelector(".popup-edit__form"));
+// let formElement = popup.map(item => item.querySelector(".popup-edit__form"));
+
 let nameInput = formElement.querySelector(".popup__text_name");
 let jobInput = formElement.querySelector(".popup__text_description");
+
 let profileName = main.querySelector(".profile__name");
 let profileJob = main.querySelector(".profile__description");
 
-// function openPopup() {
-//   popup.classList.add('popup_opened');
-//   //добавила код, который при открытии формы всегда содержит актульные данные со странички
-//   nameInput.value = profileName.textContent;
-//   jobInput.value = profileJob.textContent;
-// }
-
-// function closePopup() {
-//   popup.classList.remove('popup_opened');
-// }
-
-function openClosePopup() {
-  popup.classList.toggle("popup_opened");
-  if (popup.classList.contains("popup_opened")) {
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileJob.textContent;
-  }
+function openPopupEdit() {
+  editPopup.classList.add('popup_opened');
+  //добавила код, который при открытии формы всегда содержит актульные данные со странички
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
 }
+
+function openPopupAdd() {
+  addPopup.classList.add('popup_opened');
+}
+
+function closePopup() {
+  popup.forEach(item => item.classList.remove("popup_opened"));
+  // popup.classList.remove("popup_opened");
+}
+
+// function openClosePopup() {
+//   popup.classList.toggle("popup_opened");
+//   if (popup.classList.contains("popup_opened")) {
+//     nameInput.value = profileName.textContent;
+//     jobInput.value = profileJob.textContent;
+//   }
+// }
+
+// function openClosePopupEdit() {
+//   editPopup.classList.toggle("popup_opened");
+//   if (editPopup.classList.contains("popup_opened")) {
+//     nameInput.value = profileName.textContent;
+//     jobInput.value = profileJob.textContent;
+//   }
+// }
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -42,11 +69,15 @@ function formSubmitHandler(evt) {
   profileName.textContent = name;
   profileJob.textContent = job;
 
-  openClosePopup();
+  // openClosePopupEdit();
+  closePopup();
 }
 
-editButton.addEventListener("click", openClosePopup);
-closeButton.addEventListener("click", openClosePopup);
+editButton.addEventListener("click", openPopupEdit);
+addButton.addEventListener("click", openPopupAdd);
+
+closeButton.forEach(item => item.addEventListener("click", closePopup));
+// closeButton.addEventListener("click", closePopup);
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
